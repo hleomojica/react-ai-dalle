e10d69e52d6cbce203a0d4734de182546a97bebe
Henrry Mojica <hleomojica@gmail.com>
2023-05-25 09:07:25 +1000

    Add home

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+// create component home
+// import react
+import React from "react";
+// import useState
+import { useState } from "react";
+// import Configuration, OpenAIApi
+import { Configuration, OpenAIApi } from "openai";
+// import css
+import "./home.css";
+
+
+
+
+
